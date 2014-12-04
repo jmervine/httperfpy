259910928b4ae017e8fccd772fdbbc203469da32
Josh Mervine <jmervine@yp.com>
2014-12-04 12:11:08 -0800

    Version++

--- setup.py
+++ setup.py
@@ -2,18 +2,12 @@ from distutils.core import setup
 
 setup(
     name='httperfpy',
-    version='0.0.3',
+    version='0.0.4',
     author='Joshua P. Mervine',
     author_email='joshua@mervine.net',
     packages=['httperfpy'],
-    #scripts=['scripts/unit'],
-    #url='http://pypi.python.org/pypi/TowelStuff/',
     url='http://github.com/jmervine/httperfpy',
     license='LICENSE.txt',
     description='Python wrapper for httperf.',
     long_description=open('README.txt').read(),
-    #install_requires=[
-        ##"Django >= 1.1.1",
-        ##"caldav == 0.1.4",
-    #],
 )
